4203881476e2efa3bf9a0c972a1eb9e8f04d2275
VinGioia90 <gioiavincenzo.25790@gmail.com>
2023-07-26 12:12:26 +0200

    Small changes

--- R/summary.R
+++ R/summary.R
@@ -4,11 +4,37 @@
 #' @param object object of class mbrclm
 #' @param ... further arguments
 #'
-#' @return Summary
+#' @return The summary is printed
+#' @export summary.mbrlcm
+#' @export
 #'
+#' @name summary
+#' @examples
 summary.mbrclm <- function(object,   ...)
 {
+  print.summary.mbrclm <- function(x, digits = max(3, getOption("digits") - 3), ...)
+  {
+    #cat("\nCall:", deparse(x$call, width.cutoff = floor(getOption("width") * 0.85)), "", sep = "\n")
 
+    if(!x$convergence) {
+      cat("model did not converge\n")
+    }
+
+
+    cat(paste("\nCoefficients (with ", x$link, " link):\n", sep = ""))
+    printCoefmat(x$coefficients, digits = digits, signif.legend = FALSE)
+
+    if(getOption("show.signif.stars") & any( x$coefficients[, 4L] < 0.1))
+      cat("---\nSignif. codes: ", "0 '***' 0.001 '**' 0.01 '*' 0.05 '.' 0.1 ' ' 1", "\n")
+
+    cat("\nType of estimator:", x$typeestimator, switch(x$typeestimator,
+                                                        "AS_ml" = "(maximum likelihood)",
+                                                        "AS_mean" = "(mean bias-reduced)",
+                                                        "AS_median" = "(median bias-reduced)"))
+    cat(paste("\nNumber of iterations in the quasi-Fisher scoring:", x$iterations, "\n"))
+    cat(paste("\nConvergence status:", x$convergence, "\n"))
+    invisible(x)
+  }
   ## coefficient table
 
   cf <- as.vector(object$coefficients)
@@ -27,5 +53,6 @@ summary.mbrclm <- function(object,   ...)
 
   ## return
   class(object) <- "summary.mbrclm"
-  object
+  print.summary.mbrclm(object)
+  return(invisible(object))
 }
